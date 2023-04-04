0df5303acaa662377484cf945eb26b87b69c5bb3
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-04-04 13:17:04 +0200

    First example works in federated and centralized version
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from Model import MyNet
-from Node import Node
-from Global_Node import Global_Node
 from ArchitectureCentralize import ArchitectureCentralize
 from ArchitectureFederated import ArchitectureFederated
 from Preprocessing import Prepropressing
 
 import torch
-from torch.optim import Adam, lr_scheduler, SGD
-from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss, MSELoss
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -19,12 +15,8 @@ def main():
     # Preprocessing on the data that return a datasetTensor
     dataset = Prepropressing.first_exemple()
 
-    model = MyNet(dataset.tensors[0].shape[1:].numel(), dataset.tensors[1].shape[1:].numel())
-    criterion = BCEWithLogitsLoss()
-    optimizer_ft = Adam(model.parameters(), lr=0.001)
-    
-    architectureCentralize.start_classification(model, criterion, optimizer_ft, dataset, train_batch_size=3200, valid_batch_size=3200, num_epochs=10)
-    #architectureFederated.start_classification(model, dataset, 3, 100)
+    #architectureCentralize.start_classification(dataset)
+    architectureFederated.start_classification(dataset)
 
 
 if __name__ == "__main__":
